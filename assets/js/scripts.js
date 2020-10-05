dde4acd8297d4ca5e3146520e73ce472fb45db1c
NDRoskildel <62204276+NDRoskildel@users.noreply.github.com>
2020-10-05 19:52:32 +0300

    final ver

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,64 @@
-$(document).ready( function() { 
-    var menu__counter = false;
-    $(".header__burger").click(function(){
-        if(!menu__counter){
-            $(".menu").show();
-            menu__counter = true;
-        }else{
+$(document).ready( function() {
+    // Menu script
+    if ( $(window).width() < 993 ) {
+
+        $(".menu").hide();
+        var menu__counter = false;
+        // click burger
+        $(".header__burger").click(function(){
+            if(!menu__counter){
+                $(".menu").slideDown(500);
+                menu__counter = true;
+            }else{
+                $(".menu").slideUp(500);
+                menu__counter = false;
+            }
+        });
+        // click menu__item
+        $(".menu__item").click(function(){
+            if(!menu__counter){
+                $(".menu").slideDown(500);
+                menu__counter = true;
+            }else{
+                $(".menu").slideUp(500);
+                menu__counter = false;
+            }
+        });
+
+    }else{
+        $(".menu").css({"display":"flex"});
+    }
+
+    // Menu script if resize window
+    $(window).resize(function() {
+        if ( $(window).width() < 993 ) {
+
             $(".menu").hide();
-            menu__counter = false;
+            var menu__counter = false;
+
+            // click burger
+            $(".header__burger").click(function(){
+                if(!menu__counter){
+                    $(".menu").slideDown(500);
+                    menu__counter = true;
+                }else{
+                    $(".menu").slideUp(500);
+                    menu__counter = false;
+                }
+            });
+
+            // click menu__item
+            $(".menu__item").click(function(){
+                if(!menu__counter){
+                    $(".menu").slideDown(500);
+                    menu__counter = true;
+                }else{
+                    $(".menu").slideUp(500);
+                    menu__counter = false;
+                }
+            });
+        }else{
+            $(".menu").css({"display":"flex"});
         }
     });
-    
 });
